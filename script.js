39f70fab6f0d888607ff3d0071722367fe0fb1a8
tomoubaypasschoraSS <icaro.rocha.pereira@escola.pr.gov.br>
2026-08-13 09:52:35 -0300

    bypass=chora papai

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const botoes = document.qurySelectorAll('button')
 
 botoes.forEach(botao => {
     botao.addEventListener('click', curtir)
+    let curtiu = false
 
     function curtir() {
-        let curtidas = document.qurySelector('span')
+        let curtidas = document.querySelector('span')
+
+if (curtiu == false) {
+    curtiu = true
+    curtidas.textContent++
+    botao.style.backgroundColor = 'pink'
+}
+
+
         curtidas.textContent++
     }
 })
