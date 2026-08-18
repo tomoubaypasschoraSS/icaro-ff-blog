27f0978121df4f65f57597898f93fb9abd14ee18
tomoubaypasschoraSS <icaro.rocha.pereira@escola.pr.gov.br>
2026-08-18 09:50:38 -0300

    icaaro do bypass

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ botoes.forEach(botao => {
     let curtiu = false
 
     function curtir() {
-        let curtidas = document.querySelector('span')
+        let curtidas = botao.querySelector('span')
 
 if (curtiu == false) {
     curtiu = true
     curtidas.textContent++
     botao.style.backgroundColor = 'pink'
+}else{
+    curtiu=false
+    curtidas.textContent--
+    botao.style.backgroundColor = 'lightgray'
 }
 
 
